2a767e459941f7728e0e0cf8d761f1a7d498ec42
bookploy1928 <bookploy1928@hotmail.com>
2019-01-25 22:59:39 +0700

    tdd1

---
+++ greet.js
@@ -0,0 +1,46 @@
+function greet(name){
+    if(name === null){
+        return "Hello, my friend."
+    }
+    else if(typeof(name) ==="object"){
+       
+        if(name.length === 2){
+            return `Hello, ${name[0]} and ${name[1]}.`
+        }else{
+            //Requirement 6
+           var text = "Hello, ";
+            for (i=0; i< name.length ; i++){
+                if(name[i] == name[i].toUpperCase()){
+                    var tempText = name[i]
+                    name.splice(i, 1)
+                    for (j=0; j< name.length ; j++){
+                        if(j!= name.length -1 ){
+                            text = `${text}${name[j]} and `
+                        }else{
+                            text = `${text}${name[j]}. `
+                        }
+                    }
+                    text = `${text}AND HELLO ${tempText}!`
+                    return text
+                }
+            }
+            
+            //Requirement 5
+            const lastname = name[name.length-1];
+            delete name[name.length-1];
+            const nameSome = name.join(", ");
+            return `Hello, ${nameSome}and ${lastname}.`
+            
+
+            
+
+        
+        }
+    }
+    else if(name===name.toUpperCase()){
+        return `HELLO ${name}!`
+    }
+    return `Hello, ${name}.`; 
+}
+
+module.exports = greet;
